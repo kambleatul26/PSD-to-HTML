8b10b0726a25c0392933bb73b98ac285c11fcee8
Atul Kamble <atulkamble@localhost.com>
2019-05-22 01:03:51 +0530

    mobile view asthetics required

---
+++ indexMob.js
@@ -0,0 +1,10 @@
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+ 
